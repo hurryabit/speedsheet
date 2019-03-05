50c9872f9691252955b774cb2c81ec85ab569589
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-03-06 00:33:35 +0100

    Use typescript-formatter

--- static/speedsheet.ts
+++ static/speedsheet.ts
@@ -1,32 +1,32 @@
 // Handler for clicking on cells.
 function onActivateCell(newCoord: string) {
-  var newCell: JQuery<HTMLElement> = $('#' + newCoord);
+    var newCell: JQuery<HTMLElement> = $('#' + newCoord);
 
-  // Get old coordinate and set new coordinate.
-  var oldCoord: string = <string>($('#coord').val());
-  $('#coord').val(newCoord);
+    // Get old coordinate and set new coordinate.
+    var oldCoord: string = <string>($('#coord').val());
+    $('#coord').val(newCoord);
 
-  // Adjust highlighted cell.
-  if (!(oldCoord === '')) {
-    $('#' + oldCoord).removeClass('table-primary');
-  }
-  newCell.addClass('table-primary');
+    // Adjust highlighted cell.
+    if (!(oldCoord === '')) {
+        $('#' + oldCoord).removeClass('table-primary');
+    }
+    newCell.addClass('table-primary');
 
-  // Copy cell value into input field.
-  $('#formula').val(newCell.attr('data-formula'));
+    // Copy cell value into input field.
+    $('#formula').val(newCell.attr('data-formula'));
 }
 
 // Do this when everthing is loaded.
 $(document).ready(function() {
-  // Make log window as big as table.
-  $('#log').outerHeight($('#sheet').outerHeight());
+    // Make log window as big as table.
+    $('#log').outerHeight($('#sheet').outerHeight());
 
-  // Install handler for clicking on table cells.
-  $('td').on('click', function() { onActivateCell(this.id); });
+    // Install handler for clicking on table cells.
+    $('td').on('click', function() { onActivateCell(this.id); });
 
-  // $('#formula_form').on('submit', function(event) {
-  //   event.preventDefault();
-  //   var coord = $('#coord').val();
-  //   $('#' + coord).text($('#formula').val());
-  // });
+    // $('#formula_form').on('submit', function(event) {
+    //   event.preventDefault();
+    //   var coord = $('#coord').val();
+    //   $('#' + coord).text($('#formula').val());
+    // });
 });
