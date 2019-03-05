f2120f50c03217f59ecea02409f12df8dbf8d8a3
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-03-06 00:01:38 +0100

    Use typescript

--- static/speedsheet.js
+++ static/speedsheet.js
@@ -1,30 +1,25 @@
 // Handler for clicking on cells.
 function onActivateCell() {
-  // Get old coordinate and set new coordinate.
-  var coord = $('#coord').val();
-  $('#coord').val(this.id);
-
-  // Adjust highlighted cell.
-  if (!(coord === '')) {
-    $('#' + coord).removeClass('table-primary');
-  }
-  $(this).addClass('table-primary');
-
-  // Copy cell value into input field.
-  $('#formula').val($(this).attr('data-formula'));
+    // Get old coordinate and set new coordinate.
+    var coord = $('#coord').val();
+    $('#coord').val(this.id);
+    // Adjust highlighted cell.
+    if (!(coord === '')) {
+        $('#' + coord).removeClass('table-primary');
+    }
+    $(this).addClass('table-primary');
+    // Copy cell value into input field.
+    $('#formula').val($(this).attr('data-formula'));
 }
-
 // Do this when everthing is loaded.
-$(document).ready(function() {
-  // Make log window as big as table.
-  $('#log').outerHeight($('#sheet').outerHeight());
-
-  // Install handler for clicking on table cells.
-  $('td').on('click', onActivateCell);
-
-  // $('#formula_form').on('submit', function(event) {
-  //   event.preventDefault();
-  //   var coord = $('#coord').val();
-  //   $('#' + coord).text($('#formula').val());
-  // });
+$(document).ready(function () {
+    // Make log window as big as table.
+    $('#log').outerHeight($('#sheet').outerHeight());
+    // Install handler for clicking on table cells.
+    $('td').on('click', onActivateCell);
+    // $('#formula_form').on('submit', function(event) {
+    //   event.preventDefault();
+    //   var coord = $('#coord').val();
+    //   $('#' + coord).text($('#formula').val());
+    // });
 });
---
+++ static/speedsheet.ts
@@ -0,0 +1,30 @@
+// Handler for clicking on cells.
+function onActivateCell() {
+  // Get old coordinate and set new coordinate.
+  var coord = $('#coord').val();
+  $('#coord').val(this.id);
+
+  // Adjust highlighted cell.
+  if (!(coord === '')) {
+    $('#' + coord).removeClass('table-primary');
+  }
+  $(this).addClass('table-primary');
+
+  // Copy cell value into input field.
+  $('#formula').val($(this).attr('data-formula'));
+}
+
+// Do this when everthing is loaded.
+$(document).ready(function() {
+  // Make log window as big as table.
+  $('#log').outerHeight($('#sheet').outerHeight());
+
+  // Install handler for clicking on table cells.
+  $('td').on('click', onActivateCell);
+
+  // $('#formula_form').on('submit', function(event) {
+  //   event.preventDefault();
+  //   var coord = $('#coord').val();
+  //   $('#' + coord).text($('#formula').val());
+  // });
+});
