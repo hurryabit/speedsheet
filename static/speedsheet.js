b4872007e8592332c8853e0352ad2cc71669754f
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-03-05 22:26:26 +0100

    Remove tooltips

--- static/speedsheet.js
+++ static/speedsheet.js
@@ -1,26 +1,26 @@
+// Handler for clicking on cells.
+function onActivateCell() {
+  // Get old coordinate and set new coordinate.
+  var coord = $('#coord').val();
+  $('#coord').val(this.id);
+
+  // Adjust highlighted cell.
+  if (!(coord === '')) {
+    $('#' + coord).removeClass('table-primary');
+  }
+  $(this).addClass('table-primary');
+
+  // Copy cell value into input field.
+  $('#formula').val($(this).attr('data-formula'));
+}
+
 // Do this when everthing is loaded.
 $(document).ready(function() {
-  // Enable tooltips.
-  $('[data-toggle="tooltip"]').tooltip();
-
   // Make log window as big as table.
   $('#log').outerHeight($('#sheet').outerHeight());
 
   // Install handler for clicking on table cells.
-  $('td').on('click', function() {
-    // Get old coordinate and set new coordinate.
-    var coord = $('#coord').val();
-    $('#coord').val(this.id);
-
-    // Adjust highlighted cell.
-    if (!(coord === '')) {
-      $('#' + coord).removeClass('table-primary');
-    }
-    $(this).addClass('table-primary');
-
-    // Copy cell value into input field.
-    $('#formula').val($(this).attr('data-title'));
-  });
+  $('td').on('click', onActivateCell);
 
   // $('#formula_form').on('submit', function(event) {
   //   event.preventDefault();
