eea0f1f7ff2c7e82ddabc4ef683c1f9414502242
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-03-10 14:35:25 +0100

    Switch to yarn and start server in integration test

--- static/test.js
+++ static/test.js
@@ -1,21 +1,33 @@
 "use strict";
 const puppeteer = require("puppeteer");
+const { spawn } = require('child_process');
 
+const port = 7777;
+let server;
 let browser;
 
 beforeAll(async () => {
+  let env = process.env;
+  env.ROCKET_PORT = port;
+  server = spawn("cargo", ["run"], {
+    cwd: "..",
+    env: env,
+    stdio: "inherit",
+  });
+  await new Promise((resolve) => setTimeout(resolve, 500));
   browser = await puppeteer.launch();
 });
 
 afterAll(() => {
   browser.close();
+  server.kill("SIGTERM");
 });
 
 async function setCell(page, coord, formula) {
     const id = "#" + coord;
     await page.waitForSelector(id);
     await page.click(id);
-    const uiCoord = await page.$eval("#coord", (coordInput) => coordInput.value);
+    const uiCoord = await page.evaluate(() => document.activeElement.id);
     expect(uiCoord).toEqual(coord);
     await page.keyboard.press("Enter");
     await page.keyboard.type(new String(formula));
@@ -24,7 +36,7 @@ async function setCell(page, coord, formula) {
 
 test("1+2", async () => {
     const page = await browser.newPage();
-    await page.goto("http://localhost:8000");
+    await page.goto(`http://localhost:${port}`);
 
     await setCell(page, "A1", 1);
     await setCell(page, "A3", "A1+A2");
